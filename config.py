3f412e2ffb5f128d5375b572737d437b3279696a
PRANEET <127319827+Nani0778@users.noreply.github.com>
2024-07-05 11:10:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "7028372"))
-API_HASH = os.environ.get("API_HASH", "10bc5c7771a121c180ab8859ab438bb8")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7203878751:AAGb38yBSOYq7842PwcG24Drf-rM7D0QR7g")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", '1101776571')) 
 FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "movieplaza77")
 FSUB_GROUP = os.environ.get("FSUB_GROUP", "movieplaza77")
 CAPTION = os.environ.get("CAPTION", "")
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 SUNRISES_PIC= "https://telegra.ph/file/1daf9d172ce29199dea44.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '1101776571'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", ''))
